8f8c2b195bb0f43607abf30b4794702f18a65002
harshad576 <harshadkalariya576@gmail.com>
2021-10-11 18:23:30 +0530

    sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { FindComponent } from './find/find.component';
+import { ReportComponent } from './report/report.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "item", component: ItemsComponent },
   { path: "product", component: ProductComponent },
+  { path: "dashboard", component: DashboardComponent},
+  { path: "service", component: ServiceComponent },
+  { path: "find", component: FindComponent},
+  { path: "report", component: ReportComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServiceComponent } from './service/service.component';
+import { FindComponent } from './find/find.component';
+import { ReportComponent } from './report/report.component';
 
 
 @NgModule({
@@ -17,7 +22,12 @@ import { HeaderComponent } from './header/header.component';
     ItemsComponent,
     HomeComponent,
     ProductComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    ServiceComponent,
+    FindComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
