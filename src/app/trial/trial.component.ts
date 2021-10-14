1a9909a60a00c51bee87211ddcf85aeccf6d4e50
Hardik-Adroja <hardikadroja@gmail.com>
2021-10-14 13:44:25 +0530

    contact and trial added

---
+++ src/app/trial/trial.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-trial',
+  templateUrl: './trial.component.html',
+  styleUrls: ['./trial.component.scss']
+})
+export class TrialComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  
+
+}
