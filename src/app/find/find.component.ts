b2f239e9b321afb27701358b08a3dc0096037e1a
harshad576 <harshadkalariya576@gmail.com>
2021-10-28 16:16:16 +0530

    mapAdded

--- src/app/find/find.component.ts
+++ src/app/find/find.component.ts
@@ -1,19 +1,45 @@
+import { map } from 'rxjs/operators';
 import { UtilService } from './../common/util.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-find',
   templateUrl: './find.component.html',
   styleUrls: ['./find.component.scss']
 })
-export class FindComponent implements OnInit, OnDestroy {
+export class FindComponent implements OnInit, OnDestroy, AfterViewInit {
+  map: any;
 
-  constructor(private utilService:UtilService) { }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [22.8252, 70.8491],
+      zoom: 2
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 2,
+
+    });
+    var circle = L.circle([22.8252, 70.8491], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 5000
+    })
+      tiles.addTo(this.map);
+      circle.addTo(this.map);
+  }
+
+  constructor(private utilService: UtilService) { }
 
   ngOnInit(): void {
-    this.utilService.reportAtTime.next(["findIn",null,null])
+    this.utilService.reportAtTime.next(["findIn", null, null])
   }
-  ngOnDestroy(){
-    this.utilService.reportAtTime.next(["findOut","findIn","findTotal"])
-  }  
+  ngOnDestroy() {
+    this.utilService.reportAtTime.next(["findOut", "findIn", "findTotal"])
+  }
+  ngAfterViewInit() {
+    this.initMap();
+  }
 }
