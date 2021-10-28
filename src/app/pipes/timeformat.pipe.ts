5ec5a99108ed98557787b05e1bd78809d5690432
Hardik-Adroja <hardikadroja@gmail.com>
2021-10-28 12:48:02 +0530

    time format pipe

--- src/app/pipes/timeformat.pipe.ts
+++ src/app/pipes/timeformat.pipe.ts
@@ -5,12 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeformatPipe implements PipeTransform {
 
+  constructor() { }
+
   transform(value: number, ...args: unknown[]): unknown {
     let hour = Math.floor(value / 3600000)
     let min = Math.floor((value - (3600000 * hour)) / 60000)
-    let sec = Math.round((value - (3600000 * hour) - (60000 * min)) / 1000)
+    let sec = Math.round((value - (3600000 * hour) - (60000 * min)) / 1000);
+
     if (hour == 0 && min == 0) {
-      return `${sec} Second`;
+      return `<b>${sec}</b> Second`;
     }
     else if (hour == 0) {
       return `${min} Minute : ${sec} Second`
